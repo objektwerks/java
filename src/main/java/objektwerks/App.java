034d621c6afb4cd02897f042cf1b51b49b9c8800
objektwerks <objektwerks@runbox.com>
2022-04-23 12:39:44 -0400

    implemented logger in App

--- src/main/java/objektwerks/App.java
+++ src/main/java/objektwerks/App.java
@@ -3,7 +3,6 @@ package objektwerks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
--- src/test/java/objektwerks/BasicTest.java
+++ src/test/java/objektwerks/BasicTest.java
@@ -1,11 +1,15 @@
 package objektwerks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class BasicTest {
+    private Logger logger = LoggerFactory.getLogger(BasicTest.class);
+    
     @Test void basic() {
+        logger.info("*** BasicTest::basic: Logging is working!");
         assertEquals(1 + 1, 2);
     }
 }
