bf3ebf3f1fbfc2e69938c1f3d04d3af65257646a
objektwerks <objektwerks@runbox.com>
2022-04-23 10:10:51 -0400

    added basic unit test

---
+++ src/test/java/objektwerks/BasicTest.java
@@ -0,0 +1,12 @@
+package objektwerks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BasicTest {
+    @Test void test() {
+        assertEquals(1 + 1, 2);
+    }
+
+}
