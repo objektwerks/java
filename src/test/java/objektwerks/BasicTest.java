ebf1abab806a28bfcec74e20c26f632efdbaa410
objektwerks <objektwerks@runbox.com>
2022-04-23 15:40:04 -0400

    refactored logger in BasicTest

--- src/test/java/objektwerks/BasicTest.java
+++ src/test/java/objektwerks/BasicTest.java
@@ -1,13 +1,13 @@
 package objektwerks;
 
-import ch.qos.logback.classic.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class BasicTest {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(BasicTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(BasicTest.class);
     static {
         logger.info("*** Test logging is working!");
     }
