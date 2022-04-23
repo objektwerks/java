861fbe486fe58df3bba768f5088beaf1edeaaf1c
objektwerks <objektwerks@runbox.com>
2022-04-23 12:28:10 -0400

    added logger to App

--- src/main/java/objektwerks/App.java
+++ src/main/java/objektwerks/App.java
@@ -1,10 +1,17 @@
 package objektwerks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         String name = args.length > 0 ? args[0] : "Java geek";
         String prefix = "*** App: ";
         System.out.println(prefix + "Hello world!");
         System.out.println(prefix + "Hello, " + name + "!");
+        logger.info(prefix + "Logging is working!");
     }
 }
