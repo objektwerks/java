854c3019790e09f4e96394138e8a6ad37c91b4e0
objektwerks <objektwerks@runbox.com>
2022-04-23 12:04:07 -0400

    refactored Main

--- src/main/java/objektwerks/Main.java
+++ src/main/java/objektwerks/Main.java
@@ -2,7 +2,8 @@ package objektwerks;
 
 public class Main {
     public static void main(String[] args) {
+        String name = args.length > 0 ? args[0] : "Java geek";
         System.out.println("Hello world!");
-        System.out.println("Hello, " + args[0] + "!");
+        System.out.println("Hello, " + name + "!");
     }
 }
