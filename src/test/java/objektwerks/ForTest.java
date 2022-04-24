82fda37c1217fa359d24bf568c58b16602678a8b
objektwerks <objektwerks@runbox.com>
2022-04-24 10:03:51 -0400

    added array test

--- src/test/java/objektwerks/ForTest.java
+++ src/test/java/objektwerks/ForTest.java
@@ -3,6 +3,11 @@ package objektwerks;
 import org.junit.jupiter.api.Test;
 
 class ForTest {
+    @Test void arrayTest() {
+        int[] xs = {1, 2, 3};
+        assert(xs.length == 3);
+    }
+
     @Test void forEachTest() {
         int[] xs = {1, 2, 3};
         int sum = 0;
