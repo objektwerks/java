3944bb40ad4a50f9592cc0f8f15d3f254ed8b939
objektwerks <objektwerks@runbox.com>
2022-04-24 10:10:05 -0400

    enhanced array test

--- src/test/java/objektwerks/ForTest.java
+++ src/test/java/objektwerks/ForTest.java
@@ -1,11 +1,22 @@
 package objektwerks;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class ForTest {
     @Test void arrayTest() {
         int[] xs = {1, 2, 3};
         assert(xs.length == 3);
+
+        int[] ys = new int[3];
+        ys[0] = 1; 
+        ys[1] = 2;
+        ys[2] = 3;
+        assert(ys.length == 3);
+
+        assert(xs != ys); // reference equality
+        assert(Arrays.equals(xs, ys)); // structural equality
     }
 
     @Test void forEachTest() {
