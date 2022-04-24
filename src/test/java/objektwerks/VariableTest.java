a99d67597a2f25eacd16fc3134b1e5f4d2221d6d
objektwerks <objektwerks@runbox.com>
2022-04-24 08:47:02 -0400

    added double test; refactored test names for clarity

--- src/test/java/objektwerks/VariableTest.java
+++ src/test/java/objektwerks/VariableTest.java
@@ -3,14 +3,14 @@ package objektwerks;
 import org.junit.jupiter.api.Test;
 
 class VariableTest {
-  @Test void bool() {
+  @Test void booleanTest() {
       boolean isRound = true;
       boolean isSqaure = false;
       assert(isRound);
       assert(!isSqaure);
   }
 
-  @Test void string() {
+  @Test void stringTest() {
     String fred = "Fred Flinstone";
     String barney = "Barney Rebel";
     assert(fred != barney);
@@ -18,12 +18,21 @@ class VariableTest {
     assert(barney.length() == 12);
   }
 
-  @Test void integer() {
+  @Test void intTest() {
     int positive = 10;
     int negative = -5;
     int zero = 0; 
     assert(positive > 0 && positive == 10);
     assert(negative < 0 && negative == -5);
     assert(zero == 0);
+  }
+
+  @Test void doubleTest() {
+    double positive = 10.5;
+    double negative = -5.5;
+    double zero = 0.0; 
+    assert(positive > 0.0 && positive == 10.5);
+    assert(negative < 0.0 && negative == -5.5);
+    assert(zero == 0.0);
   }
 }
