f7155212422dc8b43d3daec7e017496c43dcbc59
objektwerks <objektwerks@runbox.com>
2022-04-24 09:41:33 -0400

    removed public modifier

--- src/test/java/objektwerks/ControlFlowTest.java
+++ src/test/java/objektwerks/ControlFlowTest.java
@@ -2,7 +2,7 @@ package objektwerks;
 
 import org.junit.jupiter.api.Test;
 
-public class ControlFlowTest {
+class ControlFlowTest {
     @Test void ifElseTest() {
         int x = 1;
         boolean result;
