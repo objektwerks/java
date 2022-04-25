c1cb8d890c4f23671ad7a1752d3fd11665d7e391
objektwerks <objektwerks@runbox.com>
2022-04-25 14:00:12 -0400

    added PatternMatchingTest

---
+++ src/test/java/objektwerks/PatternMatchingTest.java
@@ -0,0 +1,22 @@
+package objektwerks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+class PatternMatchingTest {
+    String matchNumber(Object number) {
+        return switch (number) {
+            case Integer i -> i.toString();
+            case Double d -> d.toString();
+            default -> fail("match on number failed!");
+        };
+    }
+
+    @Test void matchTest() {
+        System.out.println("*** " + matchNumber(1));
+        System.out.println("*** " + matchNumber(2.0));
+        assertEquals(matchNumber(1), "1");
+        assertEquals(matchNumber(2.0), "2.0");
+    }
+}
