7ad970bcc2a0e9d15507590fa6c12a14540c26fd
objektwerks <objektwerks@runbox.com>
2022-04-25 17:30:55 -0400

    enhanced record test

--- src/test/java/objektwerks/RecordTest.java
+++ src/test/java/objektwerks/RecordTest.java
@@ -2,20 +2,29 @@ package objektwerks;
 
 import org.junit.jupiter.api.Test;
 
-record Rectangle(double width, double height) {}
+sealed interface Shape permits Circle, Rectangle, Square {}
+record Circle(double diameter) implements Shape {}
+record Rectangle(double width, double height) implements Shape {}
+record Square(double side) implements Shape {}
 
 class RecordTest {
     @Test void recordTest() {
+        Circle circle = new Circle(6);
+        assert(circle.diameter() == 6);
+
         Rectangle rectangle = new Rectangle(2.0, 2.0);
         assert(rectangle.width() == 2.0);
         assert(rectangle.height() == 2.0);
+
+        Square square = new Square(4);
+        assert(square.side() == 4);
     }
 
     @Test void equalsTest() {
-      Rectangle rectangleA = new Rectangle(1.0, 1.0);
-      Rectangle rectangleB = new Rectangle(2.0, 2.0);
-      Rectangle rectangleC = new Rectangle(2.0, 2.0);
-      assert(!rectangleA.equals(rectangleB));
-      assert(rectangleB.equals(rectangleC));
+      Shape shapeA = new Rectangle(1.0, 1.0);
+      Shape shapeB = new Rectangle(2.0, 2.0);
+      Shape shapeC = new Rectangle(2.0, 2.0);
+      assert(!shapeA.equals(shapeB));
+      assert(shapeB.equals(shapeC));
     }
 }
