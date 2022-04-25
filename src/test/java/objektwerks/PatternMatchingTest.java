024efc3f555c5a23d3ac0ce80369707f2eee2380
objektwerks <objektwerks@runbox.com>
2022-04-25 14:13:30 -0400

    added comment

--- src/test/java/objektwerks/PatternMatchingTest.java
+++ src/test/java/objektwerks/PatternMatchingTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
+/**
+ * This feature test works with Maven and fails with Gradle. Fix unknown at this time.
+ */
 class PatternMatchingTest {
     String matchNumber(Object number) {
         return switch (number) {
