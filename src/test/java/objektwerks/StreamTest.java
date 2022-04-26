18c97d829ff272d011d72c8fb551186aa49d14d1
objektwerks <objektwerks@runbox.com>
2022-04-26 13:19:23 -0400

    added collect test

--- src/test/java/objektwerks/StreamTest.java
+++ src/test/java/objektwerks/StreamTest.java
@@ -1,13 +1,21 @@
 package objektwerks;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
 class StreamTest {
+	@Test void collectTest() {
+		var list = Stream
+				.of(1, 2, 3)
+				.map(i -> i + 1)
+				.filter(i -> i % 2 == 0).toList();
+		assert(list.equals(Arrays.asList(2, 4)));
+	}
+
 	@Test void reduceTest() {
-		var stream = Stream.of(1, 2, 3);
-		var optional = stream.map(i -> i * i).reduce(Integer::sum);
+		var optional = Stream.of(1, 2, 3).map(i -> i * i).reduce(Integer::sum);
 		assert(optional.orElse(-1) == 14);
 	}
 }
