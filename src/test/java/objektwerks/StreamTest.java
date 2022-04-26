0f6ac06c1fba8dd82cb0c4c3ef1d59f8cf983152
objektwerks <objektwerks@runbox.com>
2022-04-26 12:14:39 -0400

    implemented stream test

--- src/test/java/objektwerks/StreamTest.java
+++ src/test/java/objektwerks/StreamTest.java
@@ -1,9 +1,13 @@
 package objektwerks;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 
 class StreamTest {
 	@Test void streamTest() {
-		
+		var stream = Stream.of(1, 2, 3);
+		var sum = stream.map(i -> i * i).reduce((x, y) -> x + y);
+		assert(sum.get() == 14);
 	}
 }
