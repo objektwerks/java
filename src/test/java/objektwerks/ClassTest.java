00fa402625f763b44ac6f9063fa52f54cfacf3a9
objektwerks <objektwerks@runbox.com>
2022-04-26 07:40:37 -0400

    added var to class test

--- src/test/java/objektwerks/ClassTest.java
+++ src/test/java/objektwerks/ClassTest.java
@@ -38,18 +38,18 @@ class Dog extends Canine {
 
 class ClassTest {
     @Test void classTest() {
-        Person person = new Person("Fred Flintstone", 25);
+        var person = new Person("Fred Flintstone", 25);
         assert(person.name.equals("Fred Flintstone"));
         assert(person.age == 25);
     }
 
     @Test void inheritenceTest() {
-        Dog dog = new Dog();
+        var dog = new Dog();
         assert(dog.bark().equals("ruf ruf"));
     }
 
     @Test void interfaceTest() {
-        Dog dog = new Dog();
+        var dog = new Dog();
         assert(dog.behavior().equals("friendly"));
     }
 }
