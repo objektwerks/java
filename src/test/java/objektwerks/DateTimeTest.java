0aedca01c295b644d61c761e2d4ca3c550d2a34d
objektwerks <objektwerks@runbox.com>
2022-04-26 07:43:34 -0400

    added var to datetime test

--- src/test/java/objektwerks/DateTimeTest.java
+++ src/test/java/objektwerks/DateTimeTest.java
@@ -7,30 +7,30 @@ import org.junit.jupiter.api.Test;
 
 class DateTimeTest {
     @Test void dateTimeTest() {
-        LocalDateTime datetime = LocalDateTime.now();
+        var datetime = LocalDateTime.now();
         assert(LocalDateTime.parse(datetime.toString()).equals(datetime));
     }
     
     @Test void instantTest() {
-        Instant instant = Instant.now();
+        var instant = Instant.now();
         assert(Instant.parse(instant.toString()).equals(instant));
     }
 
     @Test void dateTest() {
-        LocalDate date = LocalDate.now();
+        var date = LocalDate.now();
         assert(LocalDate.parse(date.toString()).equals(date));
     }
 
     @Test void timeTest() {
-        LocalTime time = LocalTime.now();
+        var time = LocalTime.now();
         assert(LocalTime.parse(time.toString()).equals(time));
     }
 
     @Test void formatTest() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        LocalDateTime datetime = LocalDateTime.now();
-        String datetimeAsString = formatter.format(datetime);
-        LocalDateTime parsedDatetime = LocalDateTime.parse(datetimeAsString, formatter);
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        var datetime = LocalDateTime.now();
+        var datetimeAsString = formatter.format(datetime);
+        var parsedDatetime = LocalDateTime.parse(datetimeAsString, formatter);
         assert(datetime.getYear() == parsedDatetime.getYear());
         assert(datetime.getMonthValue() == parsedDatetime.getMonthValue());
         assert(datetime.getDayOfMonth() == parsedDatetime.getDayOfMonth());
