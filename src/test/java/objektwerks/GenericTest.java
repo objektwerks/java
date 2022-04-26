f69818792da6c7fec95180fbbedc7332bddda5f9
objektwerks <objektwerks@runbox.com>
2022-04-26 08:37:37 -0400

    added generic box and implemented generic class test

--- src/test/java/objektwerks/GenericTest.java
+++ src/test/java/objektwerks/GenericTest.java
@@ -2,9 +2,23 @@ package objektwerks;
 
 import org.junit.jupiter.api.Test;
 
+class Box<T> {
+    private T t;
+
+    void set(T t) {
+        this.t = t;
+    }
+
+    T get() {
+        return t;
+    }
+}
+
 class GenericTest {
     @Test void genericClassTest() {
-
+        var box = new Box<Integer>();
+        box.set(1);
+        assert(box.get() == 1);
     }
 
     @Test void genericMethodTest() {
