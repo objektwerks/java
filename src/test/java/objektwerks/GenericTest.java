c2b51a5dd04a0d0f6f2e9b6753594c650538fd1c
objektwerks <objektwerks@runbox.com>
2022-04-26 08:44:44 -0400

    implemented generic method test

--- src/test/java/objektwerks/GenericTest.java
+++ src/test/java/objektwerks/GenericTest.java
@@ -3,6 +3,10 @@ package objektwerks;
 import org.junit.jupiter.api.Test;
 
 class Box<T> {
+    static <T> boolean compare(Box<T> b1, Box<T> b2) {
+        return b1.get().equals(b2.get());
+    }
+
     private T t;
 
     void set(T t) {
@@ -22,6 +26,14 @@ class GenericTest {
     }
 
     @Test void genericMethodTest() {
-      
+        var box1 = new Box<Integer>();
+        var box2 = new Box<Integer>();
+        var box3 = new Box<Integer>();
+        box1.set(1);
+        box2.set(2);
+        box3.set(1);
+        assert(!Box.compare(box1, box2));
+        assert(Box.compare(box1, box3));
+
     }
 }
