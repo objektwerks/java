170bf9b9bda6d655e370af8230a25712800392b5
objektwerks <objektwerks@runbox.com>
2022-04-26 13:49:10 -0400

    edited stream test

--- src/test/java/objektwerks/StreamTest.java
+++ src/test/java/objektwerks/StreamTest.java
@@ -1,6 +1,7 @@
 package objektwerks;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,16 @@ class StreamTest {
 		var list = Stream
 				.of(1, 2, 3)
 				.map(i -> i + 1)
-				.filter(i -> i % 2 == 0).toList();
+				.filter(i -> i % 2 == 0)
+				.collect(Collectors.toList());
 		assert(list.equals(Arrays.asList(2, 4)));
 	}
 
 	@Test void reduceTest() {
-		var optional = Stream.of(1, 2, 3).map(i -> i * i).reduce(Integer::sum);
+		var optional = Stream
+				.of(1, 2, 3)
+				.map(i -> i * i)
+				.reduce(Integer::sum);
 		assert(optional.orElse(-1) == 14);
 	}
 }
