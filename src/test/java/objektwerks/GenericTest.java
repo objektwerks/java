34e77de782f40083d6fc225d0327345f5245dfa3
objektwerks <objektwerks@runbox.com>
2022-04-26 08:09:50 -0400

    added generic test

---
+++ src/test/java/objektwerks/GenericTest.java
@@ -0,0 +1,13 @@
+package objektwerks;
+
+import org.junit.jupiter.api.Test;
+
+class GenericTest {
+    @Test void genericClassTest() {
+
+    }
+
+    @Test void genericMethodTest() {
+      
+    }
+}
