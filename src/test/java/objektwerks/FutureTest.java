c0451c151221ffd2d9e1059f057057076f3a5f4b
objektwerks <objektwerks@runbox.com>
2022-04-27 07:45:03 -0400

    further simplified future test

--- src/test/java/objektwerks/FutureTest.java
+++ src/test/java/objektwerks/FutureTest.java
@@ -2,26 +2,14 @@ package objektwerks;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class FutureTest {
-    public Future<String> helloFuture() {
-        CompletableFuture<String> completableFuture = new CompletableFuture<>();
-        Executors.newCachedThreadPool().submit(() -> {
-            Thread.sleep(1000);
-            completableFuture.complete("Hello, future!");
-            return null;
-        });
-        return completableFuture;
-    }
-
     @Test void completableFutureTest() throws InterruptedException, ExecutionException {
-        var completableFuture = helloFuture();
-        var result = completableFuture.get();
+        var future = CompletableFuture.supplyAsync(() -> "Hello, future!");
+        var result = future.get();
         assertEquals("Hello, future!", result);
     }
 }
