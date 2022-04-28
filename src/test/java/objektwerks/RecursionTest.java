8f7e094de87387429d5cccff4869eff8a3bd3b38
objektwerks <objektwerks@runbox.com>
2022-04-28 17:59:40 -0400

    added product test to recursion test

--- src/test/java/objektwerks/RecursionTest.java
+++ src/test/java/objektwerks/RecursionTest.java
@@ -5,6 +5,21 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class RecursionTest {
+    int product(int[] xs, int acc) {
+        if (xs.length == 0) return acc;
+        else {
+            var from = 1;
+            var to = xs.length;
+            var head = xs[0];
+            var tail = Arrays.copyOfRange(xs, from, to);
+            System.out.println("from: " + from);
+            System.out.println("to: " + to);
+            System.out.println("head: " + head);
+            System.out.println("tail: " + Arrays.toString(tail));
+            return product(tail, acc * head);
+        }
+    }
+
     int sum(int[] xs, int acc) {
         if (xs.length == 0) return acc;
         else {
@@ -18,6 +33,13 @@ class RecursionTest {
             System.out.println("tail: " + Arrays.toString(tail));
             return sum(tail, acc + head);
         }
+    }
+
+    @Test void productTest() {
+        int[] xs = {1, 2, 3};
+        var product = product( xs, 1);
+        System.out.println("product: " + product);
+        assert(product == 6);
     }
 
     @Test void sumTest() {
