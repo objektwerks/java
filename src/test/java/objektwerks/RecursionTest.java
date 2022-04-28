4b3b2dd18e43e7d31ab4a7600e98eaccea334779
objektwerks <objektwerks@runbox.com>
2022-04-28 18:59:50 -0400

    added reverse test

--- src/test/java/objektwerks/RecursionTest.java
+++ src/test/java/objektwerks/RecursionTest.java
@@ -1,6 +1,8 @@
 package objektwerks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -47,6 +49,33 @@ class RecursionTest {
         var sum = sum( xs, 0);
         System.out.println("sum: " + sum);
         assert(sum == 6);
+    }
+
+    List<String> reverse(List<String> values, List<String> acc) {
+        if (values.isEmpty()) return acc;
+        else {
+            var from = 1;
+            var to = values.size();
+            var head = values.get(0);
+            var tail = values.subList(from, to);
+            System.out.println("from: " + from);
+            System.out.println("to: " + to);
+            System.out.println("head: " + head);
+            System.out.println("tail: " + tail);
+            acc.add(0, head);
+            return reverse(tail, acc);
+        }
+    }
+
+    @Test void reverseTest() {
+        var values = new ArrayList<String>();
+        values.add("a");
+        values.add("b");
+        values.add("c");
+        var acc = new ArrayList<String>();
+        var reversed = reverse(values, acc);
+        System.out.println("reversed: " + reversed);
+        assert(reversed.equals(List.of("c", "b", "a")));
     }
 
     int factorial(int n, int acc) {
