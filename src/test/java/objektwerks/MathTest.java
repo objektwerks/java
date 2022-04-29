199efcec949cdc5bdc9d4ca4012ea93223283c61
objektwerks <objektwerks@runbox.com>
2022-04-29 16:05:25 -0400

    enhanced math test

--- src/test/java/objektwerks/MathTest.java
+++ src/test/java/objektwerks/MathTest.java
@@ -11,15 +11,34 @@ class MathTest {
         return x - y;
     }
 
+    int multiply(int x, int y) {
+        return x * y;
+    }
+
+    int divide(int x, int y) {
+        return x / y;
+    }
+
+    boolean isEven(int n) {
+        return n  % 2 == 0;
+    }
+
+    boolean isOdd(int n) {
+        return n  % 2 != 0;
+    }
+
     @Test
     void mathTest() {
         int x = 20;
         int y = 10;
         assert(add(x, y) == 30);
         assert(subtract(x, y) == 10);
-        assert(x * y == 200);
-        assert(x / y == 2);
-        assert(x % 2 == 0 && y % 2 == 0);
+        assert(multiply(x, y) == 200);
+        assert(divide(x, y) == 2);
+        assert(isEven(x));
+        assert(isEven(y));
+        assert(!isOdd(x));
+        assert(!isOdd(y));
 
         int square = 2;
         assert(Math.pow(square, 2) == 4);
