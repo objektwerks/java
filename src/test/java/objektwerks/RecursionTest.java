ce69ee4f17f0a5cc6da19047f7c003b17ce2e806
objektwerks <objektwerks@runbox.com>
2022-04-29 07:21:46 -0400

    added fibonacci test

--- src/test/java/objektwerks/RecursionTest.java
+++ src/test/java/objektwerks/RecursionTest.java
@@ -86,4 +86,13 @@ class RecursionTest {
     @Test void factorialTest() {
         assert(factorial(3, 1) == 6);
     }
+
+    long fibonacci(long n) {
+        if ((n == 0) || (n == 1)) return n;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    @Test void fibonacciTest() {
+        assert(fibonacci(39) == 63245986);
+    }
 }
