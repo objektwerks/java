3d209d345ba90e14bb8d1a1cdf802c8b645976be
objektwerks <objektwerks@runbox.com>
2022-04-29 15:48:18 -0400

    removed random tests from variable test

--- src/test/java/objektwerks/VariableTest.java
+++ src/test/java/objektwerks/VariableTest.java
@@ -131,17 +131,4 @@ class VariableTest {
       x--;
       assert(x == 1);
   }
-
-  @Test void uuidTest() {
-      var uuid = UUID.randomUUID();
-      var nextUuid = UUID.randomUUID();
-      assert(!uuid.equals(nextUuid));
-      assert(!uuid.toString().isEmpty());
-  }
-
-  @Test void randomTest() {
-      var random = new Random();
-      assert(random.nextInt() != 0);
-      assert(Math.abs(random.nextInt()) > 0);
-  }
 }
