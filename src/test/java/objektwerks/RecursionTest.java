f66070491324d521166e9023ad7f3a25af1aad39
objektwerks <objektwerks@runbox.com>
2022-04-29 07:51:22 -0400

    reordered recursion test code

--- src/test/java/objektwerks/RecursionTest.java
+++ src/test/java/objektwerks/RecursionTest.java
@@ -7,28 +7,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class RecursionTest {
-    int product(int[] xs, int acc) {
-        if (xs.length == 0) return acc;
-        else {
-            var from = 1;
-            var to = xs.length;
-            var head = xs[0];
-            var tail = Arrays.copyOfRange(xs, from, to);
-            System.out.println("from: " + from);
-            System.out.println("to: " + to);
-            System.out.println("head: " + head);
-            System.out.println("tail: " + Arrays.toString(tail));
-            return product(tail, acc * head);
-        }
-    }
-
-    @Test void productTest() {
-        int[] xs = {1, 2, 3};
-        var product = product( xs, 1);
-        System.out.println("product: " + product);
-        assert(product == 6);
-    }
-
     int sum(int[] xs, int acc) {
         if (xs.length == 0) return acc;
         else {
@@ -49,6 +27,28 @@ class RecursionTest {
         var sum = sum( xs, 0);
         System.out.println("sum: " + sum);
         assert(sum == 6);
+    }
+
+    int product(int[] xs, int acc) {
+        if (xs.length == 0) return acc;
+        else {
+            var from = 1;
+            var to = xs.length;
+            var head = xs[0];
+            var tail = Arrays.copyOfRange(xs, from, to);
+            System.out.println("from: " + from);
+            System.out.println("to: " + to);
+            System.out.println("head: " + head);
+            System.out.println("tail: " + Arrays.toString(tail));
+            return product(tail, acc * head);
+        }
+    }
+
+    @Test void productTest() {
+        int[] xs = {1, 2, 3};
+        var product = product( xs, 1);
+        System.out.println("product: " + product);
+        assert(product == 6);
     }
 
     List<String> reverse(List<String> strings, List<String> acc) {
