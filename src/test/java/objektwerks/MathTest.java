216d360c9c8d5eb27611b90ed92cf49afa3ad36a
objektwerks <objektwerks@runbox.com>
2022-04-29 15:52:47 -0400

    formatted math test code

--- src/test/java/objektwerks/MathTest.java
+++ src/test/java/objektwerks/MathTest.java
@@ -7,11 +7,13 @@ class MathTest {
     void mathTest() {
         int x = 20;
         int y = 10;
+
         assert(x + y == 30);
         assert(x - y == 10);
         assert(x * y == 200);
         assert(x / y == 2);
         assert(x % 2 == 0 && y % 2 == 0);
+
         assert(Math.random() != 0.0);
     }
 }
