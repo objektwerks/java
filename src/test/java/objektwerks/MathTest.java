61d7f98f0b6ee7c149571efb53bdca1274278115
objektwerks <objektwerks@runbox.com>
2022-04-29 15:52:11 -0400

    added math test

---
+++ src/test/java/objektwerks/MathTest.java
@@ -0,0 +1,17 @@
+package objektwerks;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+    @Test
+    void mathTest() {
+        int x = 20;
+        int y = 10;
+        assert(x + y == 30);
+        assert(x - y == 10);
+        assert(x * y == 200);
+        assert(x / y == 2);
+        assert(x % 2 == 0 && y % 2 == 0);
+        assert(Math.random() != 0.0);
+    }
+}
