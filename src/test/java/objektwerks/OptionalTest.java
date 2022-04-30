25e713853e9457e8454b1a995c34e66a51b1de69
objektwerks <objektwerks@runbox.com>
2022-04-30 08:35:28 -0400

    enhanced optional test

--- src/test/java/objektwerks/OptionalTest.java
+++ src/test/java/objektwerks/OptionalTest.java
@@ -7,11 +7,13 @@ import java.util.Optional;
 class OptionalTest {
     @Test void emptyTest() {
         var optional = Optional.<Integer>empty();
+        assert(optional.isEmpty());
         assert(!optional.isPresent());
     }
 
     @Test void ofTest() {
         var optional = Optional.of(1);
+        assert(!optional.isEmpty());
         assert(optional.isPresent());
         assert(optional.orElse(-1) == 1);
     }
@@ -19,6 +21,7 @@ class OptionalTest {
     @Test void ofNullableTest() {
         Integer nullable = null;
         var optional = Optional.ofNullable(nullable);
+        assert(optional.isEmpty());
         assert(!optional.isPresent());
     }
 }
