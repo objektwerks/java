4e51677f7b122f7d61646d64dfbc521aacca71ad
objektwerks <objektwerks@runbox.com>
2022-04-30 08:45:55 -0400

    added or else get optional test

--- src/test/java/objektwerks/OptionalTest.java
+++ src/test/java/objektwerks/OptionalTest.java
@@ -19,6 +19,15 @@ class OptionalTest {
         assert(optional.orElse(-1) == 1);
     }
 
+    @Test void orElseGetTest() {
+        var optional = Optional.<Integer>empty();
+        assert(optional.orElseGet(() -> 1) == 1);
+        assert(optional.isEmpty());
+        assert(!optional.isPresent());
+    }
+
+
+
     @Test void ifPresentTest() {
         var optional = Optional.of(1);
         optional.ifPresent( value -> assertEquals(value, 1) );
