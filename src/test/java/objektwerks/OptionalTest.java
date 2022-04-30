6048d8793d8f3a4945112757e46fab5e83546fe8
objektwerks <objektwerks@runbox.com>
2022-04-30 08:59:23 -0400

    added get optional test; reordered code

--- src/test/java/objektwerks/OptionalTest.java
+++ src/test/java/objektwerks/OptionalTest.java
@@ -12,31 +12,30 @@ class OptionalTest {
         assert(!optional.isPresent());
     }
 
-    @Test void ofOrElseTest() {
-        var optional = Optional.of(1);
-        assert(optional.orElse(-1) == 1); // default value ALWAYS if present or empty created!
-        assert(!optional.isEmpty());
-        assert(optional.isPresent());
-    }
-
-    @Test void orElseGetTest() {
-        var optional = Optional.<Integer>empty();
-        assert(optional.orElseGet(() -> 1) == 1); // default value ONLY created when empty!
-        assert(optional.isEmpty());
-        assert(!optional.isPresent());
-    }
-
-    @Test void ifPresentTest() {
-        var optional = Optional.of(1);
-        optional.ifPresent( value -> assertEquals(value, 1) );
-        assert(!optional.isEmpty());
-        assert(optional.isPresent());
-    }
-
     @Test void ofNullableTest() {
         Integer nullable = null;
         var optional = Optional.ofNullable(nullable);
         assert(optional.isEmpty());
         assert(!optional.isPresent());
+    }
+
+    @Test void getTest() {
+        var optional = Optional.of(1);
+        assert(optional.get() == 1);
+    }
+
+    @Test void orElseTest() {
+        var optional = Optional.of(1);
+        assert(optional.orElse(-1) == 1); // default value ALWAYS if present or empty created!
+    }
+
+    @Test void orElseGetTest() {
+        var optional = Optional.<Integer>empty();
+        assert(optional.orElseGet(() -> 1) == 1); // default value ONLY created when empty!
+    }
+
+    @Test void ifPresentTest() {
+        var optional = Optional.of(1);
+        optional.ifPresent( value -> assertEquals(value, 1) );
     }
 }
