b04c441a9299d028ac5d70ff5a52750c9cd82cf4
objektwerks <objektwerks@runbox.com>
2022-05-01 05:51:47 -0400

    enhanced io test

--- src/test/java/objektwerks/IOTest.java
+++ src/test/java/objektwerks/IOTest.java
@@ -1,7 +1,10 @@
 package objektwerks;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,5 +27,11 @@ class IOTest {
         Path newFilePath = Paths.get(newFileName());
         var newFile = Files.createFile(newFilePath).toFile();
         assert(newFile.isFile());
+    }
+
+    @Test void fileOutputStreamTest() throws IOException {
+        try(var fileOutputStream = new FileOutputStream(newFileName())) {
+            fileOutputStream.write("test".getBytes(StandardCharsets.UTF_8));
+        }
     }
 }
