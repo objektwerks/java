b83d46eae78f05f4c2e7e6989738d0be866697d2
objektwerks <objektwerks@runbox.com>
2022-05-01 05:42:21 -0400

    enhanced io test

--- src/test/java/objektwerks/IOTest.java
+++ src/test/java/objektwerks/IOTest.java
@@ -2,6 +2,9 @@ package objektwerks;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -15,5 +18,11 @@ class IOTest {
         var newFile = new File(newFileName());
         boolean success = newFile.createNewFile();
         assert(success);
+    }
+
+    @Test void createFile() throws IOException {
+        Path newFilePath = Paths.get(newFileName());
+        var newFile = Files.createFile(newFilePath).toFile();
+        assert(newFile.isFile());
     }
 }
