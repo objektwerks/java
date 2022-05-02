e75d03008bd1918bcffe6235f48e3112d838ea71
objektwerks <objektwerks@runbox.com>
2022-05-02 07:35:14 -0400

    added object test

--- src/test/java/objektwerks/EqualityTest.java
+++ src/test/java/objektwerks/EqualityTest.java
@@ -2,6 +2,23 @@ package objektwerks;
 
 import org.junit.jupiter.api.Test;
 
+class Person {  // old school class, not a record
+    private final String first;
+    private final String last;
+
+    public Person(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person that = (Person) o;
+        return first.equals(that.first) && last.equals(that.last);
+    }
+}
+
 class EqualityTest {
     @Test void primitive() {
         assert(1 == 1);
@@ -21,5 +38,12 @@ class EqualityTest {
         // structural equality via equals
         assert(x.equals(y));
         assert(x.equals(z));
+    }
+
+    @Test void objectTest() {
+        var fred = new Person("fred", "flintstone");
+        var barney = new Person("barney", "rebel");
+        assert(!fred.equals(barney));
+        assert(fred.equals(new Person("fred", "flintstone")));
     }
 }
