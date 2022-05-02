69077149393f663637a137a131c1c4e2b2986308
objektwerks <objektwerks@runbox.com>
2022-05-02 09:46:30 -0400

    added http client code to App

--- src/main/java/objektwerks/App.java
+++ src/main/java/objektwerks/App.java
@@ -1,16 +1,46 @@
 package objektwerks;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static final String url = "http://api.icndb.com/jokes/random/";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String name = args.length > 0 ? args[0] : "Java geek";
         String prefix = "*** ";
+
         System.out.println(prefix + "Hello world!");
         System.out.println(prefix + "Hello, " + name + "!");
+
         logger.info(prefix + "App lgging is working!");
+
+        var client = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+        var request = HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create(url))
+            .build();
+
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var statusCode = response.statusCode();
+        var text = response.body();
+
+        System.out.println(statusCode);
+        System.out.println(text);
+
+        logger.info(prefix + "status code: " + statusCode);
+        logger.info(prefix + "text:\n" + text);
     }
 }
