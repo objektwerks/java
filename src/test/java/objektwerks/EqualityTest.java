79a22a4f7af7c7b32243612ce1add53db0dec0fe
objektwerks <objektwerks@runbox.com>
2022-05-02 07:24:46 -0400

    enhanced wrapper equality test

--- src/test/java/objektwerks/EqualityTest.java
+++ src/test/java/objektwerks/EqualityTest.java
@@ -10,7 +10,16 @@ class EqualityTest {
     @Test void wrapper() {
         var x = Integer.valueOf(1);
         var y = Integer.valueOf(1);
-        assert(x == y); // unboxes integer to int; hence the equality
+        var z = new Integer(1);
+
+        // the jvm cached x and reused it for y via Integer.valueOf; hence the structural equality
+        assert(x == y);
+
+        // x and z are tested for reference (or memory location) equality; hence the inequality
+        assert(x != z);
+
+        // structural equality via equals
         assert(x.equals(y));
+        assert(x.equals(z));
     }
 }
