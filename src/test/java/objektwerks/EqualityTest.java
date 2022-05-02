9421fe121212ad535720f6aed2ed7c2cf006a08f
objektwerks <objektwerks@runbox.com>
2022-05-02 08:25:00 -0400

    refactored equality test

--- src/test/java/objektwerks/EqualityTest.java
+++ src/test/java/objektwerks/EqualityTest.java
@@ -65,14 +65,20 @@ class EqualityTest {
         assert(!fred.equals(barney));
     }
 
-    @Test void comparatorSortTest() {
+    @Test void comparableSortTest() {
         var fred = new Person("fred", "flintstone");
         var barney = new Person("barney", "rebel");
 
         var persons = List.of(fred, barney);
         var sortedPersons = persons.stream().sorted().toList();
         assert(sortedPersons.get(0).equals(fred));
+    }
 
+    @Test void comparatorSortTest() {
+        var fred = new Person("fred", "flintstone");
+        var barney = new Person("barney", "rebel");
+
+        var persons = List.of(fred, barney);
         var byFirstComparator = Comparator.comparing(Person::first);
         var sortedPersonsByComparator = persons.stream().sorted(byFirstComparator).toList();
         assert(sortedPersonsByComparator.get(0).equals(barney));
