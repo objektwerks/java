0b8f68c30b03165fbb62e92d42885e6e2f209e02
objektwerks <objektwerks@runbox.com>
2022-05-02 07:12:07 -0400

    added wrapper equality test

--- src/test/java/objektwerks/EqualityTest.java
+++ src/test/java/objektwerks/EqualityTest.java
@@ -6,4 +6,8 @@ class EqualityTest {
     @Test void primitive() {
         assert(1 == 1);
     }
+
+    @Test void wrapper() {
+        assert(Integer.valueOf(1).equals(Integer.valueOf(1)));
+    }
 }
