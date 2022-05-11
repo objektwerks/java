d8b39850e7a2f80b2807d9c9185fc141c3d21268
objektwerks <objektwerks@runbox.com>
2022-05-11 08:00:44 -0400

    added object pass by value test

--- src/test/java/objektwerks/PassByValueTest.java
+++ src/test/java/objektwerks/PassByValueTest.java
@@ -2,14 +2,23 @@ package objektwerks;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Stream;
+
 /**
  * <a href="https://www.baeldung.com/java-pass-by-value-or-pass-by-reference">Java Pass By Value</a>
  * 1. For Primitive types, parameters are pass-by-value.
  * 2. For Object types, the object reference is pass-by-value.
  */
 class PassByValueTest {
-    int square(int i) {
+    int square(int i) { // passed by value as primitive
         return i * i; // returns new primitive by value
+    }
+
+    String join(String... values) { // passed by value as references
+        return Stream
+                .of(values)
+                .reduce(String::concat)
+                .orElse(""); // returns pass by value reference to new String
     }
 
     @Test void primitiveTest() {
@@ -18,6 +27,7 @@ class PassByValueTest {
     }
 
     @Test void objectTest() {
-
+        var joined = join("a", "b", "c");
+        assert(joined.equals("abc"));
     }
 }
