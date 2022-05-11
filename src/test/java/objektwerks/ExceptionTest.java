856187f8d836de2f41fab1aebf9c31ff8f57c457
objektwerks <objektwerks@runbox.com>
2022-05-11 07:27:46 -0400

    added checked exception test

--- src/test/java/objektwerks/ExceptionTest.java
+++ src/test/java/objektwerks/ExceptionTest.java
@@ -1,11 +1,20 @@
 package objektwerks;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ExceptionTest {
     @Test void checkedTest() {
-
+        assertThrows(IOException.class, () -> {
+            var path = Paths.get("build.gradle");
+            var lines = Files.readAllLines(path);
+            assert(!lines.isEmpty());
+        });
     }
 
     @Test void uncheckedTest() {
