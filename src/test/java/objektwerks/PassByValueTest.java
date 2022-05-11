e6ce62c830d0b8326b0769bacf80a0cc3c86a491
objektwerks <objektwerks@runbox.com>
2022-05-11 07:54:03 -0400

    added pass by value test

---
+++ src/test/java/objektwerks/PassByValueTest.java
@@ -0,0 +1,18 @@
+package objektwerks;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * <a href="https://www.baeldung.com/java-pass-by-value-or-pass-by-reference">Java Pass By Value</a>
+ * 1. For Primitive types, parameters are pass-by-value.
+ * 2. For Object types, the object reference is pass-by-value.
+ */
+class PassByValueTest {
+    @Test void primitiveTest() {
+
+    }
+
+    @Test void objectTest() {
+
+    }
+}
