4f207141494db1548745e83ac61b8869dde8c313
objektwerks <objektwerks@runbox.com>
2022-10-30 07:54:09 -0400

    added virtual thread test stub

---
+++ src/test/java/objektwerks/VirtualThreadTest.java
@@ -0,0 +1,14 @@
+package objektwerks;
+
+import org.junit.jupiter.api.Test;
+
+class VirtualThreadTest {
+    long fibonacci(long n, long a, long b) {
+        if (n == 0) return a;
+        else return fibonacci(n -1, b, a + b);
+    }
+
+    @Test void threadTest() {
+        assert(fibonacci(39, 0, 1) == 63245986);
+    }
+}
