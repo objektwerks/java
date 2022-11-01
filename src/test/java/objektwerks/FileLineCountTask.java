99b55a2a0fe841e37dfe52a6d4885112a7f22cb4
objektwerks <objektwerks@runbox.com>
2022-11-01 08:07:32 -0400

    added FileLineCountTask

---
+++ src/test/java/objektwerks/FileLineCountTask.java
@@ -0,0 +1,29 @@
+package objektwerks;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Callable;
+
+class FileLineCountTask implements Callable<Integer> {
+    String file;
+
+    Integer fileLineCount(String file) {
+        try {
+            var path = Paths.get(file);
+            return Files.readAllLines(path).size();
+        } catch(IOException ioe) {
+            return -1;
+        }
+
+    }
+
+    public FileLineCountTask(String file) {
+        this.file = file;
+    }
+
+    @Override
+    public Integer call() {
+        return fileLineCount(file);
+    }
+}
