cff5b4ca10d89d67d8923a63c4f3dbdcce42ff2b
objektwerks <objektwerks@runbox.com>
2023-03-28 07:16:09 -0400

    edited VirtualThreadTest

--- src/test/java/objektwerks/VirtualThreadTest.java
+++ src/test/java/objektwerks/VirtualThreadTest.java
@@ -7,6 +7,8 @@ import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
 
 /**
+ * Run > Edit Configurations > VirtualThreadTest
+ * --enable-preview --add-modules jdk.incubator.concurrent
  * Virtual Threads: openjdk.org/jeps/425
  * Article: www.marcobehler.com/guides/java-project-loom
  */
@@ -16,7 +18,7 @@ class VirtualThreadTest {
         tasks.add( new FileLineCountTask("./data/data.a.csv") );
         tasks.add( new FileLineCountTask("./data/data.b.csv") );
 
-        int lines = 0;
+        var lines = 0;
         try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
             var futures = executor.invokeAll(tasks);
             for (var future : futures) {
