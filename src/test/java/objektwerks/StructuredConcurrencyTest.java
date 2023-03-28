557f983e644557835026de1a0551944879b84e12
objektwerks <objektwerks@runbox.com>
2023-03-28 07:12:35 -0400

    edited StructuredConcurrencyTest

--- src/test/java/objektwerks/StructuredConcurrencyTest.java
+++ src/test/java/objektwerks/StructuredConcurrencyTest.java
@@ -1,9 +1,6 @@
 package objektwerks;
 
 import jdk.incubator.concurrent.StructuredTaskScope;
-import jdk.incubator.concurrent.ScopedValue;
-
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -13,20 +10,8 @@ import org.junit.jupiter.api.Test;
  * Run > Edit Configurations > StructuredConcurrencyTest
  * --enable-preview --add-modules jdk.incubator.concurrent
  * Structured Concurrency: openjdk.org/jeps/428
- * ScopedValue: openjdk.org/jeps/429 ( TODO )
  */
 class StructuredConcurrencyTest {
-    private static final String uuid = UUID.randomUUID().toString();
-    private static final ScopedValue<String> license = ScopedValue.newInstance();
-
-    @Test void scopedValueTest() throws Exception {
-        Integer lines = ScopedValue
-                            .where(license, uuid)
-                            .call( new FileLineCountTask("./data/data.a.csv") );
-        assert(lines == 270562);
-        assert(!license.isBound());
-    }
-
     @Test void structuredConcurrencyTest() throws ExecutionException, InterruptedException {
         int lines;
         try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
