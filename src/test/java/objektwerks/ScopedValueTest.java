86c048dfdf98ef27a7f7df4b5dd623bdfa14b8b0
objektwerks <objektwerks@runbox.com>
2023-03-28 07:09:55 -0400

    added ScopedValueTest

---
+++ src/test/java/objektwerks/ScopedValueTest.java
@@ -0,0 +1,25 @@
+package objektwerks;
+
+import jdk.incubator.concurrent.ScopedValue;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+/**
+ * Run > Edit Configurations > ScopedValueTest
+ * --enable-preview --add-modules jdk.incubator.concurrent
+ * Structured Concurrency: openjdk.org/jeps/428
+ * ScopedValue: openjdk.org/jeps/429 ( TODO )
+ */
+class ScopedValueTest {
+    private static final String uuid = UUID.randomUUID().toString();
+    private static final ScopedValue<String> license = ScopedValue.newInstance();
+
+    @Test void scopedValueTest() throws Exception {
+        Integer lines = ScopedValue
+                .where(license, uuid)
+                .call( new FileLineCountTask("./data/data.a.csv") );
+        assert(lines == 270562);
+        assert(!license.isBound());
+    }
+}
