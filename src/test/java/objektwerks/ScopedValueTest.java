0aeb4aed07c8a2d3fc71b2db0439c8ab0baccb1f
objektwerks <objektwerks@runbox.com>
2023-11-01 16:29:06 -0400

    removed import and javadoc

--- src/test/java/objektwerks/ScopedValueTest.java
+++ src/test/java/objektwerks/ScopedValueTest.java
@@ -2,15 +2,8 @@ package objektwerks;
 
 import java.util.UUID;
 
-import jdk.incubator.concurrent.ScopedValue;
-
 import org.junit.jupiter.api.Test;
 
-/**
- * Run > Edit Configurations > ScopedValueTest
- * --enable-preview --add-modules jdk.incubator.concurrent
- * ScopedValue: openjdk.org/jeps/429
- */
 class ScopedValueTest {
     private static final ScopedValue<String> license = ScopedValue.newInstance();
     private static final String uuid = UUID.randomUUID().toString();
