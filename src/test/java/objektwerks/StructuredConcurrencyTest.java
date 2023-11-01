a6df859b76c475a7cab38aa22b80857ae0157820
objektwerks <objektwerks@runbox.com>
2023-11-01 16:34:26 -0400

    refactored structured concurrency test

--- src/test/java/objektwerks/StructuredConcurrencyTest.java
+++ src/test/java/objektwerks/StructuredConcurrencyTest.java
@@ -1,25 +1,19 @@
 package objektwerks;
 
-import jdk.incubator.concurrent.StructuredTaskScope;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.StructuredTaskScope;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Run > Edit Configurations > StructuredConcurrencyTest
- * --enable-preview --add-modules jdk.incubator.concurrent
- * Structured Concurrency: openjdk.org/jeps/428
- */
 class StructuredConcurrencyTest {
     @Test void structuredConcurrencyTest() throws ExecutionException, InterruptedException {
         int lines;
         try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            Future<Integer> alines = scope.fork(() -> new FileLineCountTask("./data/data.a.csv").call());
-            Future<Integer> blines = scope.fork(() -> new FileLineCountTask("./data/data.b.csv").call());
+            var alines = scope.fork(() -> new FileLineCountTask("./data/data.a.csv").call());
+            var blines = scope.fork(() -> new FileLineCountTask("./data/data.b.csv").call());
             scope.join();
             scope.throwIfFailed();
-            lines = alines.resultNow() + blines.resultNow();
+            lines = alines.get() + blines.get();
         }
         assert(lines == 540_959);
     }
