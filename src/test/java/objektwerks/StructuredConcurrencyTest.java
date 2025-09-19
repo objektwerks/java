325d9c6c6f107226ae91deca68e110a3074f11af
objektwerks <objektwerks@runbox.com>
2025-09-19 06:41:20 -0400

    corrected structured concurrency test

--- src/test/java/objektwerks/StructuredConcurrencyTest.java
+++ src/test/java/objektwerks/StructuredConcurrencyTest.java
@@ -8,11 +8,10 @@ import org.junit.jupiter.api.Test;
 class StructuredConcurrencyTest {
     @Test void structuredConcurrencyTest() throws ExecutionException, InterruptedException {
         int lines;
-        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+        try (var scope = StructuredTaskScope.open()) {
             var alines = scope.fork(() -> new FileLineCountTask("./data/data.a.csv").call());
             var blines = scope.fork(() -> new FileLineCountTask("./data/data.b.csv").call());
             scope.join();
-            scope.throwIfFailed();
             lines = alines.get() + blines.get();
         }
         assert(lines == 540_959);
